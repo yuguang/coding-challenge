a92b30b9b4ed046dded2737f464ee77589a91e5b
yuguang <zh.yuguang.cn@gmail.com>
2015-10-31 15:27:31 -0400

    add code to clean output

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,1 +1,22 @@
 # example of program that calculates the number of tweets cleaned
+import settings, os, json
+num_cleaned = 0
+output_lines = []
+
+def encode_ascii(input):
+    output_chars = []
+    for character in input:
+        if ord(character) < 128:
+            output_chars.append(character)
+    return ''.join(output_chars)
+
+with open(os.path.join(settings.INPUT_DIR, 'tweets.txt')) as input_file:
+    for tweet in input_file:
+        tweet_dict = json.loads(tweet)
+        cleaned_text = encode_ascii(tweet_dict['text'])
+        output_lines.append('{} (timestamp: {})'.format(cleaned_text, tweet_dict['created_at']))
+        if len(cleaned_text) != len(tweet_dict['text']):
+            num_cleaned += 1
+
+print(output_lines)
+print(num_cleaned)
