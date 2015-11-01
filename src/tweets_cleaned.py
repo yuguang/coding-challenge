c5d2cb08be71562c1d67da63046e08b1836ec941
yuguang <zh.yuguang.cn@gmail.com>
2015-10-31 20:42:49 -0400

    write cleaned tweets to file

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -14,9 +14,11 @@ with open(os.path.join(settings.INPUT_DIR, 'tweets.txt')) as input_file:
     for tweet in input_file:
         tweet_dict = json.loads(tweet)
         cleaned_text = encode_ascii(tweet_dict['text'])
-        output_lines.append('{} (timestamp: {})'.format(cleaned_text, tweet_dict['created_at']))
+        output_lines.append('{} (timestamp: {})\n'.format(cleaned_text, tweet_dict['created_at']))
         if len(cleaned_text) != len(tweet_dict['text']):
             num_cleaned += 1
 
-print(output_lines)
-print(num_cleaned)
+
+with open(os.path.join(settings.OUTPUT_DIR, 'ft1.txt'), 'w') as output_file:
+    output_file.writelines(output_lines)
+    output_file.write('{} tweets contained unicode.'.format(num_cleaned))
