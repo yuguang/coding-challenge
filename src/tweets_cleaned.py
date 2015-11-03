5417c5ee41ca8631b8ab7f7b32e97bc402c28d63
yuguang <zh.yuguang.cn@gmail.com>
2015-11-03 09:25:57 -0500

    write to output file incrementally

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -1,7 +1,6 @@
 # example of program that calculates the number of tweets cleaned
 import settings, os, json
 num_cleaned = 0
-output_lines = []
 
 def encode_ascii(input):
     output_chars = []
@@ -10,15 +9,14 @@ def encode_ascii(input):
             output_chars.append(character)
     return ''.join(output_chars)
 
-with open(os.path.join(settings.INPUT_DIR, 'tweets.txt')) as input_file:
+with open(os.path.join(settings.INPUT_DIR, 'tweets.txt')) as input_file, open(os.path.join(settings.OUTPUT_DIR, 'ft1.txt'), 'w') as output_file:
     for tweet in input_file:
         tweet_dict = json.loads(tweet)
         cleaned_text = encode_ascii(tweet_dict['text'])
-        output_lines.append('{} (timestamp: {})\n'.format(cleaned_text, tweet_dict['created_at']))
+        output_file.write('{} (timestamp: {})\n'.format(cleaned_text, tweet_dict['created_at']))
         if len(cleaned_text) != len(tweet_dict['text']):
             num_cleaned += 1
 
 
-with open(os.path.join(settings.OUTPUT_DIR, 'ft1.txt'), 'w') as output_file:
-    output_file.writelines(output_lines)
+with open(os.path.join(settings.OUTPUT_DIR, 'ft1.txt'), 'a') as output_file:
     output_file.write('{} tweets contained unicode.'.format(num_cleaned))
