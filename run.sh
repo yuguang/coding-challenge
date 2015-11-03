52dab71a60b825971853bf68467915f171a2b637
yuguang <zh.yuguang.cn@gmail.com>
2015-11-03 09:21:38 -0500

    call python processing scripts

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # example of the run script for running the word count
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-chmod +w ./tweet_output/ft1.txt
-python3 ./src/words_tweeted.py
-python3 ./src/median_unique.py
+chmod +w ./tweet_output/ft*.txt
+python2 ./src/tweets_cleaned.py
+python2 ./src/average_degree.py
 
 
 
